6c128f69fd4b5bc9e0e7359b3160a1dc7eea833b
donagema <52147287+donagema@users.noreply.github.com>
2019-06-24 12:09:13 +0700

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable =['slug','photo','name','description','stock','price','category_id','user_id'];
+
+    // 
+    function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
