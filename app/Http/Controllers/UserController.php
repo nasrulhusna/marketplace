36e07f63d9d30a839c269cc133426927190ffd83
nasrulhusna <50608623+nasrulhusna@users.noreply.github.com>
2019-06-26 14:06:21 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,4 +12,78 @@ class UserController extends Controller
     	$user = User::all();
     	return view('admin.user.index',compact('user'));
     }
+    public function changestatus($id)
+    {
+    	 $user = User::find($id);
+    	if($user->status==0){
+    		$change = '1';
+    	}else{
+    		$change = '0';
+    	}
+
+    	User::where('id',$id)->update(['status' => $change]);
+    	Alert::success('Success Message', 'Status Berhasil Diubah');
+    	return redirect('admin/user');
+    }
+
+    public function create()
+    {
+    	return view('admin.user.add');
+    }
+
+    public function store(Request $data)
+    {
+    	$mydata = ([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'username' => $data['username'],
+            'address' => $data['address'],
+            'phone' => $data['phone'],
+            'gender' => $data['gender'],
+            'birthday' => $data['birthday'],
+            'role' => $data['role'],
+            'status' => "0",
+
+
+            'password' => Hash::make($data['password']),
+        ]);
+        User::create($mydata);
+        Alert::success('Success Message', 'User Berhasil Disimpan');
+    	return redirect('admin/user');
+    }
+
+    public function edit($id)
+    {
+    	$user = User::find($id);
+    	return view('admin.user.edit',compact('user'));
+    }
+    public function update(Request $data)
+    {
+
+    	$mydata = ([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'username' => $data['username'],
+            'address' => $data['address'],
+            'phone' => $data['phone'],
+            'gender' => $data['gender'],
+            'birthday' => $data['birthday'],
+            'role' => $data['role'],
+            'status' => "0",
+
+
+            'password' => Hash::make($data['password']),
+        ]);
+        User::where('id',$data->id)->update($mydata);
+        Alert::success('Success Message', 'User Berhasil Diedit');
+    	return redirect('admin/user');
+    }
+
+    public function delete($id)
+    {
+    	$user = User::find($id);
+    	$user->delete();
+    	Alert::success('Success Message', 'User Berhasil Dihapus');
+    	return redirect('admin/user');
+    }
 }
